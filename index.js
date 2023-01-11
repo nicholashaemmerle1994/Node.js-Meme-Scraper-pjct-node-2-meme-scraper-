5f85720622b7548b44f029c2c9810826692f3175
Nicholas Hämmerle <nicholas.haemmerle@gmx.at>
2023-01-11 20:58:56 +0100

    Added fs and comments

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import * as cheerio from 'cheerio';
+import fs from 'fs';
 import request from 'request';
 
 //  Origin link for from the meme page we get the data from
 const origin = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-//  function to fetch the whole html body if there is a connection to the website,
-//  if there is a response
-
+//  The Array with the requested first 10 links to the imgs.
 const linkArray = [];
 
+//  Variable of the folder that will be deleted
+const dir =
+  '/Users/nicholashammerle/projects/node.js-meme-scraper-pjct-node-2-meme-scraper-/memes';
+
+//  funtion to delete the folder from the last time the programm was started
+fs.rmdir(dir, () => {
+  console.log(`${dir} is deleted!`);
+});
+
+//  function to fetch the whole html body if there is a connection to the website,
+//  if there is a response
 request(origin, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const content = cheerio.load(body);
@@ -19,6 +29,14 @@ request(origin, (error, response, body) => {
         linkArray.push(link);
       }
     });
-    console.log(linkArray);
+    const folderName =
+      '/Users/nicholashammerle/projects/node.js-meme-scraper-pjct-node-2-meme-scraper-/memes';
+    try {
+      if (!fs.existsSync(folderName)) {
+        fs.mkdirSync(folderName);
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 });
