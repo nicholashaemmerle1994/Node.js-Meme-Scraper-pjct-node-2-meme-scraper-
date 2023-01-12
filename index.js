f8d716745d6f6c7f826f53ba063fca6dbe0567e2
Nicholas Hämmerle <nicholas.haemmerle@gmx.at>
2023-01-12 15:15:51 +0100

    Fixed the code and cleaned up

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
 import fs from 'node:fs';
-import * as cheerio from 'cheerio';
 import client from 'node:https';
+import * as cheerio from 'cheerio';
 import request from 'request';
 
-//  Origin link for from the meme page we get the data from
 const origin = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-//  The Array with the requested first 10 links to the imgs.
-const linkArray = [];
+const linkArray = []; //  includes the first 10 img src´s from the origin website
 
-//  Variable of the folder that will be deleted
-const dir =
-  '/Users/nicholashammerle/projects/node.js-meme-scraper-pjct-node-2-meme-scraper-/memes';
+const dir = './memes'; //  Variable of the folder that will be deleted and created
 
-//  funtion to delete the folder from the last time the programm was started
-fs.rmdir(dir, () => {
-  console.log(`${dir} is deleted!`);
-});
+fs.rmdir(dir, () => {}); // this deletes the memes folder from the last time the program ran
 
 //  function to fetch the whole html body if there is a connection to the website,
 //  if there is a response
 request(origin, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    const content = cheerio.load(body);
+    const htmlContent = cheerio.load(body);
+
     //  loops through every div > a > picks the src and places it in the linkArray
-    content('div a img').each((i, element) => {
+    htmlContent('div a img').each((i, element) => {
       if (i <= 9) {
-        const link = content(element).attr('src');
+        const link = htmlContent(element).attr('src');
         linkArray.push(link);
       }
     });
-    const folderName =
-      '/Users/nicholashammerle/projects/node.js-meme-scraper-pjct-node-2-meme-scraper-/memes';
     try {
-      if (!fs.existsSync(folderName)) {
-        fs.mkdirSync(folderName);
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
       }
     } catch (err) {
       console.error(err);
     }
-
     //  Here should start the loop that downloads the imgs and puts it in a new file inside the meme folder
+    for (let i = 0; i < linkArray.length; i++) {
+      client.get(linkArray[i], (res) => {
+        let dir2 = `./memes/0${i + 1}.jpg`;
+        res.pipe(fs.createWriteStream(dir2));
+      });
+    }
   }
 });
-// let url =
-//   "https://api.memegen.link/images/rollsafe/can't_get_fired/if_you_don't_have_a_job.jpg?width=300";
-// let filepath =
-//   '/Users/nicholashammerle/projects/node.js-meme-scraper-pjct-node-2-meme-scraper-/memes/01.jpeg';
-
-// function downloadImage(url, filepath) {
-//   client.get(url, (res) => {
-//     res.pipe(fs.createWriteStream(filepath));
-//   });
-// }
-// downloadImage(url, filepath);
